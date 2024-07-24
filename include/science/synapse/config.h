c799e10dae16f643e7abbd90538b7428fb7ecd0c
Antonia Elsen <antoniaelsen@gmail.com>
2024-07-24 14:07:01 -0700

    feat: add Config::nodes(), Config::connections()

--- include/science/synapse/config.h
+++ include/science/synapse/config.h
@@ -15,7 +15,7 @@ class Device;
 
 /**
  * Configuration for a Synapse device.
- * 
+ *
  * Contains configuration for any nodes on the Device, as well as their connections.
  * A Device must be configured before its signal chain can be run.
  */
@@ -26,41 +26,55 @@ class Config {
   /**
    * Add multiple nodes to the configuration.
    * The nodes will be assigned IDs.
-   * 
+   *
    * @param nodes Node configurations to add
-   * @return science::Status 
+   * @return science::Status
    */
   [[nodiscard]] auto add(std::vector<std::shared_ptr<Node>> nodes) -> science::Status;
 
   /**
    * Add a node to the configuration.
    * The node will be assigned an ID.
-   * 
+   *
    * @param node A node configuration to add
-   * @return science::Status 
+   * @return science::Status
    */
   [[nodiscard]] auto add_node(std::shared_ptr<Node> node) -> science::Status;
 
   /**
    * Connect the output of one node to the input of another.
    * Both nodes must be added to the Config before they can be connected.
-   * 
+   *
    * @param src The source node.
    * @param dst The destination node.
-   * @return science::Status 
+   * @return science::Status
    */
   [[nodiscard]] auto connect(std::weak_ptr<Node> src, std::weak_ptr<Node> dst) -> science::Status;
 
   /**
+   * Get the connections in the configuration.
+   *
+   * @return const std::vector<std::pair<uint64_t, uint64_t>>&
+   */
+  [[nodiscard]] auto connections() const -> const std::vector<std::pair<uint64_t, uint64_t>>&;
+
+  /**
+   * Get the nodes in the configuration.
+   *
+   * @return const std::vector<std::shared_ptr<Node>>&
+   */
+  [[nodiscard]] auto nodes() const -> const std::vector<std::shared_ptr<Node>>&;
+
+  /**
    * Convert the configuration to its corresponding DeviceConfiguration proto message.
-   * 
-   * @return synapse::DeviceConfiguration 
+   *
+   * @return synapse::DeviceConfiguration
    */
   auto to_proto() -> synapse::DeviceConfiguration;
 
   /**
    * Convert the given DeviceConfiguration proto to its corresponding Config instance, with nodes and connections.
-   * 
+   *
    * @param proto The DeviceConfiguration proto message.
    * @param config The Config instance to populate.
    * @return science::Status
