fe7ebd38a123f4fca875730c0a6c3b9a1e4d139f
Antonia Elsen <antoniaelsen@gmail.com>
2024-10-17 18:00:02 -0700

    chore: add binned spike parsing to example

--- examples/stream_out/main.cpp
+++ examples/stream_out/main.cpp
@@ -7,11 +7,12 @@
 #include "science/synapse/nodes/electrical_broadband.h"
 #include "science/synapse/nodes/stream_out.h"
 
+using synapse::BinnedSpiketrainData;
 using synapse::Ch;
 using synapse::ElectricalBroadbandData;
 using synapse::SynapseData;
 
-auto stream(const std::string& uri, const std::string& group) -> int {
+auto stream(const std::string& uri, const std::string& group, bool configure) -> int {
   synapse::Device device(uri);
   synapse::Config config;
   science::Status s;
@@ -24,31 +25,38 @@ auto stream(const std::string& uri, const std::string& group) -> int {
   }
   std::cout << "device info: " << info.DebugString() << std::endl;
 
-  auto stream_out = std::make_shared<synapse::StreamOut>("out", group);
+  std::shared_ptr<synapse::StreamOut> stream_out;
+  if (configure) {
+    // Configure the device to stream out electrical broadband data
+    std::vector<synapse::Ch> channels;
+    for (unsigned int i = 0; i < 19; i++) {
+      channels.push_back(synapse::Ch{
+        .id = i,
+        .electrode_id = i * 2,
+        .reference_id = i * 2 + 1,
+      });
+    }
+    auto electrical_broadband = std::make_shared<synapse::ElectricalBroadband>(
+        1, channels, 16, 30000, 20.0, 500, 6000);
+    s = config.add_node(stream_out);
+    s = config.add_node(electrical_broadband);
+    s = config.connect(electrical_broadband, stream_out);
 
-  std::vector<synapse::Ch> channels;
-  for (unsigned int i = 0; i < 19; i++) {
-    channels.push_back(synapse::Ch{
-      .id = i,
-      .electrode_id = i * 2,
-      .reference_id = i * 2 + 1,
-    });
+    s = device.configure(&config);
+
+    s = device.start();
+
+  } else {
+    // Read data from a device that is already configured
+    stream_out = std::make_shared<synapse::StreamOut>("out", group);
+    s = config.add_node(stream_out);
+    s = config.set_device(&device);
   }
-  auto electrical_broadband = std::make_shared<synapse::ElectricalBroadband>(
-    1, channels, 16, 30000, 20.0, 500, 6000);
-  s = config.add_node(stream_out);
-  s = config.add_node(electrical_broadband);
-  s = config.connect(electrical_broadband, stream_out);
-
-  s = device.configure(&config);
-
-  s = device.start();
 
   while (true) {
     SynapseData out;
     s = stream_out->read(&out);
 
-    std::cout << "-" << std::endl;
     if (!s.ok()) {
       std::cout << "error reading from stream out node: ("
         << static_cast<int>(s.code()) << ") " << s.message() << std::endl;
@@ -72,6 +80,24 @@ auto stream(const std::string& uri, const std::string& group) -> int {
           std::cout << "     - sample [" << i << "]: " << c.channel_data[i] << std::endl;
         }
       }
+
+    } else if (std::holds_alternative<BinnedSpiketrainData>(out)) {
+      auto data = std::get<BinnedSpiketrainData>(out);
+      size_t n_spiking_channels = 0;
+      std::stringstream ss;
+      for (size_t i = 0; i < data.spike_counts.size(); i++) {
+        if (data.spike_counts[i] > 0) {
+          if (n_spiking_channels > 0) {
+            ss << ", ";
+          }
+          ss << "[" << i << "]: " << static_cast<unsigned int>(data.spike_counts[i]);
+          n_spiking_channels++;
+        }
+      }
+      if (n_spiking_channels > 0) {
+        std::cout << " - received " << n_spiking_channels << " spiking channels:" << std::endl;
+        std::cout << ss.str() << std::endl;
+      }
     } else {
       std::cout << "data type unknown" << std::endl;
     }
@@ -82,8 +108,8 @@ auto stream(const std::string& uri, const std::string& group) -> int {
 
 int main(int argc, char** argv) {
   std::string uri = "192.168.0.1:647";
-  std::string group = "239.0.0.234";
-  stream(uri, group);
+  std::string group = "239.0.0.1";
+  stream(uri, group, false);
 
   return 0;
 }
